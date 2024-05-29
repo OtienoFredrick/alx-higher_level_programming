87de07d75890cc8802f38904253806befeb7c9d4
marshall235 <fredie1738@gmail.com>
2024-05-29 20:14:51 +0300

    The command creates the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Crates the database hbtn_0c_0 in the mysql server
+-- If the database exists, the script should not fail
+-- Not allowed to use the select or the show statement
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
