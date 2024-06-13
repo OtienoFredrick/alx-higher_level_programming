7f20cdc2789ba3a08139f414844edd7934d16d76
marshall235 <fredie1738@gmail.com>
2024-06-13 21:21:23 +0300

    a script that lists the number of records with the same score in the table second_table

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- A script that lists the number of records with the same score
+-- in the table second table of the database hbtn_0c_0
+
+SELECT DISTINCT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY NUMBER;
