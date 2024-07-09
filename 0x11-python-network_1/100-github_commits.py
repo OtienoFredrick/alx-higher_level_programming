dfd7b6e5cdcc58a44d6ffaaa86a4a233be9cff81
marshall235 <fredie1738@gmail.com>
2024-07-09 20:28:49 +0300

    a Python script that takes 2 arguments in order to solve this challenge.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,6 +5,7 @@ Usage: ./100-github_commits.py <repository name> <repository owner>
 import sys
 import requests
 
+
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
             sys.argv[2], sys.argv[1])
@@ -17,4 +18,3 @@ if __name__ == "__main__":
                 commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
-
