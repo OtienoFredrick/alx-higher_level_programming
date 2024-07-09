02c49683a27bd0669de7ae6bf155da7209347b91
marshall235 <fredie1738@gmail.com>
2024-07-09 19:59:43 +0300

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" A python script that takes in a letter then sends
-a post request to http://0.0.0.0:5000/search_user
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
 import sys
 import requests
@@ -8,7 +10,7 @@ import requests
 
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload  = {"q": letter}
+    payload = {"q": letter}
 
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
