3433656fbfbe275abd203f2e058fda26bdcb62b9
marshall235 <fredie1738@gmail.com>
2024-07-19 19:03:24 +0300

    a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-""" The script lists all cities from database
-hbtn_0e_4_usa
 """
-import MySQLdb
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
+
+import MySQLdb as db
 from sys import argv
 
-
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
-with db.cursor() as cursor:
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.state_id \
-            = states.id ORDER BY cities.id ASC")
-    rows = cursor.fetchall()
+    """
+    Access the database and get the cities
+    from the database.
+    """
 
-    if rows is not None:
-        for row in rows:
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        rows_selected = db_cursor.fetchall()
+
+    if rows_selected is not None:
+        for row in rows_selected:
             print(row)
