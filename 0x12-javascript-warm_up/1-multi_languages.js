238a4626c0a336de3f27c46cb9615e22fbc094d3
marshall235 <fredie1738@gmail.com>
2024-07-20 18:56:56 +0300

    a script that prints 3 lines:
    The first line: “C is fun”
    The second line: “Python is cool”
    The third line: “JavaScript is amazing”

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myVar = ["C is fun", "Python is cool", "Javascript is amazing"];
+const myVar = ['C is fun', 'Python is cool', 'JaVascript is amazing'];
 for (let i = 0; i < myVar.length; i++)
 {
 	console.log(myVar[i]);
