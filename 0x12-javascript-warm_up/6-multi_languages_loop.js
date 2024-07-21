1dba76737aaa40469ffa9ded235c21e6c7cb38c4
marshall235 <fredie1738@gmail.com>
2024-07-21 16:32:52 +0300

    a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
    The first line: “C is fun”

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let c = 0; c < myVar.length; c++)
-{
-	console.log(myVar[c]);
-}
+myVar.forEach((element) => {
+	console.log(element);
+});
