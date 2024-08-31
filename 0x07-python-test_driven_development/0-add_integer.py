f3cb719b1cfa57e838127dc588f0b9e35819b981
marshall235 <fredie1738@gmail.com>
2024-08-31 12:46:44 +0300

    A python code that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
 
-This module has one function that adds up to 2 integers
+This module has one function that adds up 2 integers
 
 """
 
 
 def add_integer(a, b=98):
-    """ Return the sum of two integers or floats as integers
+    """Return the sum of two integers or floats as integers
 
     Args:
         a: first argument
-        b: Second argument
+        b: second argument
 
     Returns:
-        TypeError: If either of the arguments not an integer or a float
+        Sum of the two arguments
 
     Raises:
-        TypeError: If either of the arguments not an integer of a float
+        TypeError: If either of the arguments not an integer or a float
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
