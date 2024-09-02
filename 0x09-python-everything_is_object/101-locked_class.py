c1757c4c08c00f3b0bf9ced867ce11b5d7cdd97c
marshall235 <fredie1738@gmail.com>
2024-09-02 16:17:07 +0300

    Python Project everything is an object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a locked class"""
+
+
+class LockedClass:
+    """
+    Allows only instatiation of an attribute called first_name
+    """
+
+    __slots__ = ["first_name"]
