7847138d2e8f3748bd65964d0726c34c5e840c1c
FREDRICK OTIENO <fredie1738@gmail.com>
2025-01-18 10:19:25 +0300

    Update 16-no_link.sql
    spelling error

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- A scropt that lists all records of the table 
+-- A script that lists all records of the table 
 -- of the database hbtn_0c_0
 -- Don’t list rows without a name value
 -- Results should display the score and the name (in this order)
